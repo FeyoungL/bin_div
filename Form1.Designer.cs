d3a8c4f5a5e7bf0638cd4a19a7cd00bac8b6e86d
GeniTop_Fey <GeniTop_Fey@DESKTOP-DJNK1PA>
2019-10-23 17:57:06 +0800

    FeyL

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,52 +32,89 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(575, 137);
+            this.button1.Location = new System.Drawing.Point(691, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "打开文件";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(575, 216);
+            this.button2.Location = new System.Drawing.Point(691, 216);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "拆分文件";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(174, 139);
+            this.textBox1.Location = new System.Drawing.Point(447, 139);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(282, 21);
+            this.textBox1.Size = new System.Drawing.Size(209, 21);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(174, 218);
+            this.textBox2.Location = new System.Drawing.Point(447, 216);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(282, 21);
+            this.textBox2.Size = new System.Drawing.Size(209, 21);
             this.textBox2.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(408, 426);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(447, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "起始地址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(449, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "结束地址1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +126,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,22 +17,120 @@ namespace bin_div
             InitializeComponent();
         }
 
+        OpenFileDialog dialog = new OpenFileDialog();
+        FileStream fs = null;
+        BinaryReader binReader = null;
+
+        String fileName = String.Empty;
+        String richText = String.Empty;
+
+        private void errorText(String str)
+        {
+            richTextBox1.ForeColor = Color.Red;
+            richTextBox1.Text = "ERROR: " + str + System.Environment.NewLine;
+        }
+
+        private void infoText(String str)
+        {
+
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-            String fileName = String.Empty;
-            OpenFileDialog dialog = new OpenFileDialog();
             dialog.Multiselect = false; //不能多个文件
             dialog.Title = "请选择文件";
             dialog.Filter = "bin文件(*.bin)|*.bin|所有文件(*.*)|*.*";
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                textBox1.Text = dialog.FileName;
-                System.IO.Stream fileStream = dialog.OpenFile();
-                using (StreamReader sr = new StreamReader(fileStream))
-                {
-                    textBox2.Text = sr.ReadLine();
-                }
+                //textBox1.Text = dialog.FileName;    //显示名字
+                
+                fileName = dialog.FileName;
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                binReader = new BinaryReader(fs);
+                //textBox2.Text = fs.Length.ToString();     //获取长度
+
+                //foreach (byte j in binReader.ReadChars(200))
+                //{
+                //    richText += j.ToString("X2");
+                //    richText += " ";
+                //}
+                //richTextBox1.Text = richText;   //打印
+
+                richTextBox1.ForeColor = Color.Black;
+                richTextBox1.Text += "Load file: " + fileName + " success." + System.Environment.NewLine;
+
+
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            long addrStart = 0;
+            long addrEnd = 0;
+            if(String.IsNullOrEmpty(fileName))
+            {
+                errorText("Please open bin file to split operation.");
+                return;
+            }
+            if (String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text))
+            {
+                errorText("addr is null or Empty.");
+                return;
+            }
+            try
+            {
+                addrStart = long.Parse(textBox1.Text);
+                addrEnd = long.Parse(textBox2.Text);
+                if(addrEnd<=addrStart)
+                {
+                    errorText("The start of Address >= The end of Address.");
+                    return;
+                }
+            }
+            catch
+            {
+                errorText("addr is not number.");
+                return;
+            }
+            richTextBox1.ForeColor = Color.Black;
+            richTextBox1.Text = "ok."+ System.Environment.NewLine;
+            if (binReader != null)
+            {
+                binReader.Close();
+            }
+            if(fs != null)
+            { 
+                fs.Close();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            if(binReader != null)
+            {
+                binReader.Close();
+            }
+            if(fs != null)
+            {
+                fs.Close();
+            }
+        }
+
     }
 }
